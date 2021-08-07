7b92aefd146836bd80ff2edfb496ccfa7be504c0
TigRig <tigrig.crex@gmail.com>
2021-08-07 13:25:00 +0900

    データの追加、および表示

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import API from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub'
+import awsconfig from './aws-exports'
 
-createApp(App).mount('#app')
+API.configure(awsconfig)
+PubSub.configure(awsconfig)
+
+const app = createApp(App)
+app.mount('#app')
